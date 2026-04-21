6bc3c69cc6b59df90277ad364de527fc209567a3
feliciahornesjo <hornesjo@hotmail.com>
2026-04-21 15:46:37 +0200

    Added product related classes

--- src/application/ProductService.java
+++ src/application/ProductService.java
@@ -1,14 +1,22 @@
 package application;
 import domain.Product;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductService {
+    private List<Product> products = new ArrayList<>();
 
-    public void createProduct(String productName){
-        Product product = new Product(productName, "default", 10);
-        System.out.println("Product created: " + product.getProductName());
+    public CreateProductResult createProduct(CreateProductRequest request) {
+        Product product = new Product(
+            request.getProductName(),
+            request.getProductCategory(),
+            request.getEstimatedLifespan()
+    );
+
+    products.add(product);
+
+    return new CreateProductResult(product.getProductName());
 
     }
-
-
 
 }
